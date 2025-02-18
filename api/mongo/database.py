3413c3c49abc909932b35e64e082e7bf3993df5c
Bianca Malta <92928037+BiancaMalta@users.noreply.github.com>
2025-02-18 19:13:15 -0300

    Delete api/mongo directory

--- api/mongo/database.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")
-db = client["your_database_name"]
-
-def get_collection(collection_name):
-    return db[collection_name]
--- api/mongo/models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class Movie(BaseModel):
-    title: str
-    genre: str
-    year: int
-    rating: float
